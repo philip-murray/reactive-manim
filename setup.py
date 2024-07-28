ef5e30db5e048939112372bb6154c8a5f35a64f3
philip-murray <philipppaulmurray@gmail.com>
2024-07-28 14:28:54 +0200

    restructuring project for pip 4

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='dynamic-manim-components',
-    version='0.1.1',
+    version='0.1.2',
     description='A ManimCE component library, supporting component composition and automatic animation',
     author='Philip Murray',
     author_email='philipmurray.code@gmail.com',
     url='https://github.com/philip-murray/dynamic-manim-components',
-    packages=find_packages(where='dynamic_manim_components'),
-    package_dir={'': 'dynamic_manim_components'},
+    packages=find_packages(),
     install_requires=[
         'manim>=0.18.1', 
     ],
