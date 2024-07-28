8409ce137f0f7910723e505154c575f030ebb1a1
philip-murray <philipppaulmurray@gmail.com>
2024-07-28 22:33:53 +0200

    restructuring project for pip 6

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='dynamic-manim-components',
     version='0.1.3',
@@ -7,14 +10,11 @@ setup(
     author='Philip Murray',
     author_email='philipmurray.code@gmail.com',
     url='https://github.com/philip-murray/dynamic-manim-components',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=['dynamic_manim_components', 'dynamic_manim_components.src', 'dynamic_manim_components.src.manim_src'],
     install_requires=[
         'manim>=0.18.1', 
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
     ],
     python_requires='>=3.8',
 )
